c6f9c66fcb66ff0f9298a80bb67f7c695e146300
Alwin Sunu <alwinsunu1974@gmail.com>
2026-02-13 19:37:58 +0530

    Update ComplexAddition.java

--- src/ComplexAddition.java
+++ src/ComplexAddition.java
@@ -3,56 +3,36 @@ import java.util.Scanner;
 class Complex {
     int real;
     int imag;
-    
-    Complex(int real,int imag){
-        this.real=real;
-        this.imag=imag;
-        
-        
-    }
-    int add(Complex c1,Complex c2){
 
-        Complex c3=new Complex(c1.real+c2.real,c1.imag+c2.imag);
-       
-        return c3;
+    Complex(int real, int imag) {
+        this.real = real;
+        this.imag = imag;
     }
-    // TODO: parameterized constructor Complex(int real, int imag)
 
-    // TODO: Method to add two complex numbers: add(Complex other) returning new Complex
-    // Or static method add(Complex c1, Complex c2)
+    // Static method to add two complex numbers
+    static Complex add(Complex c1, Complex c2) {
+        return new Complex(c1.real + c2.real, c1.imag + c2.imag);
+    }
 }
 
 public class ComplexAddition {
-    public void main(String args[]){
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
 
-        Scanner sc=new Scanner(System.in);
-        int a;
-        int b;
-        int c;
-        int d;
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
 
-        a=sc.NextInt();
-        b=sc.NextInt();
-        c=sc.NextInt();
-        d=sc.NextInt();
-        Complex c1=new Complex(a,b);
-        Complex c2=new Complex(c,d);
+        Complex c1 = new Complex(a, b);
+        Complex c2 = new Complex(c, d);
 
-        //add(c1,c2);
+        // Using the static method from the Complex class
+        Complex c3 = Complex.add(c1, c2);
 
-        Complex c3=add(c1,c2);
-
-        System.out.println(c3.real+" "+c3.imag+"i");
-
-
+        // Printing in the requested format
+        System.out.println(c3.real + " + " + c3.imag + "i");
+        
+        sc.close();
     }
-        // TODO: Read real1, imag1
-        // TODO: Read real2, imag2
-        
-        // TODO: Create two Complex objects
-        
-        // TODO: Add them
-        
-        // TODO: Print result in "real + imagi" format (e.g., "6 + 8i")
-    
 }
